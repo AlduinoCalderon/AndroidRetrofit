9496c8fb41278974950f9a97e17baa1b81978494
AlduinoCalderon <127164974+AlduinoCalderon@users.noreply.github.com>
2024-10-13 18:45:03 -0600

    an

--- app/src/main/java/com/example/androidretrofit/VistaResultadosActivity.java
+++ app/src/main/java/com/example/androidretrofit/VistaResultadosActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidretrofit;
 
+import android.app.NotificationChannel;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -8,13 +9,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import com.example.androidretrofit.ResultadosAdapter;
 import com.example.androidretrofit.dataaccess.ConexionRetrofit;
 import com.example.androidretrofit.dataaccess.VistaResultadosAPI;
 import com.example.androidretrofit.databinding.ActivityVistaResultadosBinding;
 import com.example.androidretrofit.model.VistaResultados;
-
+import android.content.Intent;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
 import java.util.List;
 import java.util.ArrayList;
 import retrofit2.Call;
@@ -56,7 +63,32 @@ public class VistaResultadosActivity extends AppCompatActivity {
         binding.recyclerView1.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView1.setAdapter(adapter);
     }
+    private void showNotification(String message) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Verifica si el dispositivo está ejecutando Android 8.0 (Oreo) o superior
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("channel_id", "Nombre del Canal", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        // Intent para abrir la actividad al hacer clic en la notificación
+        Intent resultIntent = new Intent(this, VistaResultadosActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Crea la notificación
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+                .setSmallIcon(R.drawable.ic_launcher_background) // Reemplaza con tu propio icono de notificación
+                .setContentTitle("Título de la notificación")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(resultPendingIntent)
+                .setAutoCancel(true);
+
+        // Muestra la notificación
+        notificationManager.notify(1, builder.build());
+    }
     private void makeApiCall(String nombre) {
 
         Call<ArrayList<VistaResultados>> call = vistaResultadosAPI.getVistaResultadosById(nombre);
@@ -67,8 +99,9 @@ public class VistaResultadosActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     ArrayList<VistaResultados> vistaResultadosList = response.body();
                     updateUI(vistaResultadosList);
+                    showNotification("Consulta exitosa");
                 } else {
-                    Toast.makeText(VistaResultadosActivity.this, "Error en la respuesta de la API", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(VistaResultadosActivity.this, "Error en la respuesta de la API. El registro no existe.", Toast.LENGTH_SHORT).show();
                 }
             }
 
